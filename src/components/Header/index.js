087dacc675e6391dcb97b46b9e95513fd71d2077
KRVPerera <rukshan.viduranga@gmail.com>
2021-07-24 10:32:25 +0530

    BreadCrumb.styles.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import Router from 'react';
+import {Link} from 'react-router-dom';
 
 // logos
 import RMDBLogo from '../../images/react-movie-logo.svg'
@@ -10,7 +11,9 @@ import {Wrapper, Content, LogoImg, TMDBLogoImg} from "./Header.styles"
 const Header = () => (
     <Wrapper>
         <Content>
-            <LogoImg src={RMDBLogo} alt='rmdb-logo-image'/>
+            <Link to='/'>
+                <LogoImg src={RMDBLogo} alt='rmdb-logo-image'/>
+            </Link>
             <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo-image'/>
         </Content>
     </Wrapper>
--- src/components/Thumb/index.js
+++ src/components/Thumb/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 // styles
 import {Image} from './Thumb.styles';
 
 const Thumb = ({image, alt_message, movieId, clickable }) => (
     <div>
-        <Image src={image} alt={"Movie poster missing : '" + alt_message + "'"} />
+        {clickable ? (
+            <Link to={`/${movieId}`}>
+                <Image src={image} alt={"Movie poster missing : '" + alt_message + "'"} />
+            </Link>
+        ) : (
+            <Image src={image} alt={"Movie poster missing : '" + alt_message + "'"} />
+        )}
     </div>
 )
 
