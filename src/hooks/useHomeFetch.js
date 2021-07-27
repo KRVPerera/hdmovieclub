8dbef6e7b0a7d020ca6166a890cd7aca19c55776
KRVPerera <rukshan.viduranga@gmail.com>
2021-07-27 21:44:49 +0530

    Keep home page info on session storage

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,5 +1,8 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState} from "react"
 import API from "../API";
+
+// helpers
+import {isPersistedState} from "../helpers"
 
 const initialState = {
     page: 0,
@@ -35,6 +38,14 @@ export const useHomeFetch = () => {
 
     // initial render and search
     useEffect(() => {
+        if (!searchTerm) {
+            const sessionState = isPersistedState('homeState')
+            if (sessionState) {
+                setState(sessionState)
+                return
+            }
+        }
+
         setState(initialState)
         fetchMovies(1, searchTerm)
     }, [searchTerm])
@@ -47,6 +58,11 @@ export const useHomeFetch = () => {
         setIsLoadingMore(false);
 
     }, [isLoadingMore, searchTerm, state.page])
+
+    // write to sessionStorage
+    useEffect(() => {
+        if (!searchTerm) sessionStorage.setItem('homeState', JSON.stringify(state))
+    }, [searchTerm, state])
 
     return {state, loading, error, searchTerm, setSearchTerm, setIsLoadingMore}
 }
