f8286bbcb421e67a3a3db9096fddfa329f7ddd9a
KRVPerera <rukshan.viduranga@gmail.com>
2021-08-23 14:57:39 +0530

    Fix search bar style

--- src/components/SearchBar/SearchBar.styles.js
+++ src/components/SearchBar/SearchBar.styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   display: flex;
-  align-items: center;
   background: var(--clr-900);
-  padding: 10px 20px;
+  padding: 5px 50px 5px 10px;
+  margin: 0 auto;
 `;
 
 export const Content = styled.div`
@@ -13,37 +13,41 @@ export const Content = styled.div`
   width: 100%;
   height: 50px;
   background: var(--clr-800);
+  align-content: center;
   margin: 0 auto;
   border-radius: 40px;
   color: var(--white);
 
   img {
-    position: absolute;
-    left: 15px;
-    top: 14px;
-    width: 30px;
+    margin: 13px 15px;
+    width: 25px;
   }
 
   input {
     font-size: var(--fontBig);
     position: absolute;
-    left: 0;
-    margin: 8px 0;
-    padding: 0 0 0 60px;
+    left: 45px;
+    padding: 15px 0 0 10px;
     border: 0;
     width: 95%;
     background: transparent;
-    height: 40px;
+    border-radius: 60px;
     color: var(--clr-50);
 
     :focus {
       position: absolute;
-      width: 93%;
+      padding: 15px 0 0 10px;
+      margin: 0 0;
       border-radius: 60px;
-      padding: 0 0 0 60px;
-      height: 40px;
-      left: 55px;
+      width: 94%;
     }
+  }
+  
+  input[type=text]
+  {
+    height: 95%;
+    line-height: 30px;
+    padding:0 10px;
   }
 `;
 
